a6ce4416a596031ce67ee2a5c790728a99b69111
BeBan09 <beckersbenedikt@gmail.com>
2020-07-18 16:58:00 +0200

    - Dummheit added 4.0 (srls ich hab kp was ich geadded habe)

--- bot/bot.py
+++ bot/bot.py
@@ -10,18 +10,32 @@ def test():
 
 
 def print_help():
-    out = 'Hier ist eine Liste der Commands: \n'
+    out = 'Hier ist eine Beispielliste von Commands: \n'
     for key in cmds.keys():
         out = '{}{}\n'.format(out, key)
     return out
 
 
-def change_name(name):
-    return ''
+def change_name():
+    name = input('Zu was soll dein Name geändert werden?\n')
+    with open('data.json', 'r') as json_file:
+        json_data = json_file.read()
+        obj = json.loads(json_data)
+    with open('data.json', 'w') as json_file:
+        obj['name'] = name
+        json.dump(obj, json_file)
+    return 'Name geändert zu ' + name
 
 
-def change_age(age):
-    return ''
+def change_age():
+    age = input('Zu was soll dein Alter geändert werden?\n')
+    with open('data.json', 'w') as json_file:
+        json_data = json_file.read()
+        obj = json.loads(json_data)
+    with open('data.json', 'w') as json_file:
+        obj['age'] = age
+        json.dump(obj, json_file)
+    return 'Alter geändert zu ' + age
 
 
 cmds = {
@@ -29,7 +43,7 @@ cmds = {
     'bye': platzhalter,
     'test': test,
     'help': print_help,
-    'hilfe': print_help
+    'hilfe': print_help,
 }
 
 
@@ -38,11 +52,13 @@ def check(msg):
     output = ''
     if 'stop' in msg or 'bye' in msg:
         return 'Stoppt', 'stop'
+    if 'change' in msg and 'name' in msg:
+        return change_name(), ''
+    if 'change' in msg and 'age' in msg:
+        return change_age(), ''
     for d in cmds.keys():
         if d in msg:
             output = f'{cmds[d]()}\n{output}'
     if output == '':
         output = 'Unbekannter Befehl :( \nBenutze help für Hilfe'
     return output, ''
-
-
